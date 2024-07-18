b17d4ff7e9626d10dfbc18e53e20498dfbf3a2a6
Evžen Wybitul <wybitul.evzen@gmail.com>
2024-07-18 15:26:48 +0000

    Update prediction code

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -2,37 +2,36 @@
 #!%load_ext autoreload
 #!%autoreload 2
 
+import datetime
+from pathlib import Path
+
 import datasets
+import jsonlines
 import polars as pl
 import torch
+import torch.nn.functional as F
 from peft import PeftModel
 from polars import col as c
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 # %%
 model_id = "google/flan-t5-large"
 
-model = AutoModel.from_pretrained(model_id)
-model = PeftModel.from_pretrained(model, "Eugleo/results")
+device = "cuda:0"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_id).to(device)
+model = PeftModel.from_pretrained(model, "Eugleo/results").to(device)
 
 model.eval()
 
 # %%
-data_path = "/Users/eugen/Downloads/Projects/emotion-in-political-language/data/processed/*.parquet"
-
-data = pl.read_parquet(data_path)
-
-dataset = datasets.Dataset.from_parquet(
-    "/Users/eugen/Downloads/Projects/emotion-in-political-language/data/processed/*.parquet",
-    streaming=True,
-)
-
+dataset = datasets.load_dataset(
+    "Eugleo/us-congressional-speeches", streaming=True
+).filter(lambda x: 64 / 1.5 < len(x["text"]) < 1024 / 1.5)
 
 # %%
-
-
 # Initialize tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
@@ -40,48 +39,65 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 def collate_fn(batch):
     return {
         "speech_id": [item["speech_id"] for item in batch],
+        "date": [item["date"] for item in batch],
+        "chamber": [item["chamber"] for item in batch],
         "text": [item["text"] for item in batch],
     }
 
 
 @torch.inference_mode()
-def get_predictions(texts):
-    inputs = tokenizer(texts, truncation=True, padding=True, return_tensors="pt")
-    outputs = model(**inputs)
-    probabilities = outputs.logits[:, 0]
+def get_predictions(texts, device):
+    inputs = tokenizer(texts, truncation=True, padding=True, return_tensors="pt").to(
+        device
+    )
+    outputs = model(
+        input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+    )
+    probabilities = outputs.logits.softmax(dim=-1)[:, 0]
     return probabilities
 
 
 # Create DataLoader
-dataloader = DataLoader(dataset, batch_size=32, collate_fn=collate_fn)
+dataloader = DataLoader(dataset["train"], batch_size=128, collate_fn=collate_fn)
 
 # Create a list to store the results
-results = []
+speech_ids, dates, chambers, probabilities = [], [], [], []
 
-# Process the dataset
-for batch in tqdm(dataloader):
-    probabilities = get_predictions(batch["text"])
+with torch.inference_mode(), jsonlines.open("predictions.jsonl", "w") as writer:
+    # Process the dataset
+    for batch in tqdm(dataloader):
+        probs = get_predictions(batch["text"], device)
 
-    results += [
-        {"speech_id": speech_id, "prob_emotional": prob}
-        for speech_id, prob in zip(batch["speech_id"], probabilities)
-    ]
+        speech_ids.extend(batch["speech_id"])
+        dates.extend(batch["date"])
+        chambers.extend(batch["chamber"])
+        probabilities.extend(probs.tolist())
 
-
-# Convert results to a Polars DataFrame
-predictions_df = pl.DataFrame(results)
-
-# Convert the IterableDataset to a Polars DataFrame
-
-# Join predictions with the original dataset
-final_df = data.join(predictions_df, on="speech_id")
-
+        for speech_id, date, chamber, probability in zip(
+            speech_ids, dates, chambers, probabilities
+        ):
+            writer.write(
+                {
+                    "speech_id": speech_id,
+                    "date": str(date),
+                    "chamber": chamber,
+                    "probability": probability,
+                }
+            )
 # %%
 from lets_plot import *
 
-timeseries = final_df.group_by("chamber", "date").agg(
-    c("prob_emotional").mean().alias("mean_prob_emotional"),
-    (c("prob_emotional") > 0.5).sum().alias("count_emotional"),
+LetsPlot.setup_html()
+
+predictions_df = pl.scan_ndjson(
+    "predictions.jsonl",
+    schema_overrides={"date": pl.Datetime},
+)
+
+
+timeseries = predictions_df.group_by("chamber", "date").agg(
+    c("probability").mean().alias("mean_prob_emotional"),
+    (c("probability") > 0.5).sum().alias("count_emotional"),
 )
 
 (
