513788346ed794f4f05a7e0c718bc327640f0fd3
eugleo <wybitul.evzen@gmail.com>
2024-07-20 14:47:51 -0700

    Add the possibility to submit multiple batches in parallel

--- scripts/model_pairwise_labels.py
+++ scripts/model_pairwise_labels.py
@@ -5,12 +5,15 @@ import sys
 from pathlib import Path
 from typing import Annotated, Optional
 
+import datasets
 import jsonlines
+import numpy as np
 import openai
 import polars as pl
 import typer
 from dotenv import load_dotenv
 from polars import col as c
+from rich.pretty import pprint
 
 # Add the project root directory to the Python path
 project_root = Path(__file__).resolve().parent.parent
@@ -23,33 +26,23 @@ app = typer.Typer()
 DEFAULT_BATCH_DIR = Path("data/batch/")
 
 
-def cost_estimate(texts: list[str], token_chars: int = 4, token_cost=0.000005) -> float:
+def cost_estimate(
+    texts: list[str], token_chars: int = 4, token_cost=0.00000015
+) -> float:
     return sum(len(text) / token_chars for text in texts) * token_cost
 
 
-def sample_pairs(
-    num: int,
-    seed: int,
-    word_limit: tuple[float, float],
-    path: Path,
-) -> pl.DataFrame:
-    ids = (
-        pl.scan_parquet(path)
-        .filter(c("word_count") > word_limit[0], c("word_count") < word_limit[1])
-        .select("speech_id")
-        .collect()
-        .get_column("speech_id")
-        .sample(num * 2, seed=seed)
-    )
-
-    examples = pl.scan_parquet(path).filter(c("speech_id").is_in(ids)).collect()
+def sample_pairs(num: int, dataset: datasets.Dataset) -> pl.DataFrame:
+    ids = list(range(len(dataset)))
+    chosen_ids = np.random.choice(ids, num * 2, replace=False)
+    examples = dataset.select(chosen_ids)
 
     result = pl.DataFrame(
         {
-            "speech_1": examples[:num].get_column("text"),
-            "speech_1_id": examples[:num].get_column("speech_id"),
-            "speech_2": examples[num:].get_column("text"),
-            "speech_2_id": examples[num:].get_column("speech_id"),
+            "speech_1": examples[:num]["text"],
+            "speech_1_id": examples[:num]["speech_id"],
+            "speech_2": examples[num:]["text"],
+            "speech_2_id": examples[num:]["speech_id"],
         }
     )
 
@@ -118,32 +111,16 @@ Excerpt 1: {excerpt_1}
 
 The goal is to judge which of these two, if any, is more emotional. Don't take the topic itself into account. Sometimes you will need to read between the lines and infer the emotionality level, e.g. in cases when the text is passive aggresive. Is there any difference between Excerpt 1 and Excerpt 2 in terms of emotionality? If there is not, say so.
 
-Only after you’re done with the analysis, end the message with "result: 0" (without quotes) if Excerpt 0 is the more emotional, or "result: 1" if Excerpt 1 is more emotional. Use "result: -1"  if the result is unclear or there is no large difference.
+Only after you're done with the analysis, end the message with "result: 0" (without quotes) if Excerpt 0 is the more emotional, or "result: 1" if Excerpt 1 is more emotional. Use "result: -1"  if the result is unclear or there is no large difference.
 """[1:]
 
 
-@app.command()
-def submit(
-    num: Annotated[int, typer.Option(min=1)],
-    batch_dir: Annotated[
-        Path, typer.Option(exists=True, file_okay=False, dir_okay=True)
-    ] = DEFAULT_BATCH_DIR,
-    model: Annotated[str, typer.Option()] = "gpt-4o",
-    data_path: Annotated[Path, typer.Option(file_okay=False, dir_okay=True)] = Path(
-        "data/processed/*.parquet"
-    ),
-    min_words: Annotated[int, typer.Option(min=1)] = 64,
-    max_words: Annotated[int, typer.Option(min=1)] = 1024,
-    seed: Annotated[int, typer.Option()] = 42,
-):
-    utils.set_seed(seed)
-
+def process_batch(batch_dir: Path, pairs: pl.DataFrame, model: str):
     id = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     batch_dir = batch_dir / id
     batch_dir.mkdir(parents=True, exist_ok=True)
     batch_path = batch_dir / "batch.jsonl"
 
-    pairs = sample_pairs(num, seed, (min_words, max_words), data_path)
     pairs.write_parquet(batch_dir / "input.parquet")
     texts = write_batch_file(
         speech_pairs=pairs,
@@ -152,9 +129,6 @@ def submit(
         model=model,
     )
 
-    print(f"Estimated cost: ${cost_estimate(texts):.2f}")
-
-    load_dotenv()
     client = openai.OpenAI()
     batch_file = client.files.create(file=open(batch_path, "rb"), purpose="batch")
 
@@ -169,17 +143,35 @@ def submit(
         json.dump(batch.to_dict(), f)
 
     print(f"Batch {batch.id} created.")
+    print(f"Estimated cost: ${cost_estimate(texts):.2f}")
 
 
 @app.command()
-def download(
+def submit(
+    num: Annotated[int, typer.Argument(min=1)],
     batch_dir: Annotated[
-        Optional[Path], typer.Argument(exists=True, file_okay=False, dir_okay=True)
-    ] = None,
+        Path, typer.Option(exists=True, file_okay=False, dir_okay=True)
+    ] = DEFAULT_BATCH_DIR,
+    model: Annotated[str, typer.Option()] = "gpt-4o-mini",
+    dataset_id: Annotated[
+        str, typer.Option("--dataset")
+    ] = "Eugleo/us-congressional-speeches-subset",
+    seed: Annotated[int, typer.Option()] = 42,
 ):
-    if batch_dir is None:
-        batch_dir = max(list(DEFAULT_BATCH_DIR.glob("*")), key=lambda x: x.name)
+    load_dotenv()
+    utils.set_seed(seed)
 
+    dataset = datasets.load_dataset(dataset_id)["train"]  # type: ignore
+    assert isinstance(dataset, datasets.Dataset)
+
+    pairs = sample_pairs(num, dataset)
+
+    batch_size = 50_000
+    for i in range(0, len(pairs), batch_size):
+        process_batch(batch_dir, pairs[i : i + batch_size], model)
+
+
+def download_batch(batch_dir: Path):
     with open(batch_dir / "request.json") as f:
         request = json.load(f)
 
@@ -188,7 +180,8 @@ def download(
     batch = client.batches.retrieve(request["id"])
     if batch.status != "completed":
         print(f"Batch {batch.id} is not completed yet.")
-        print(batch)
+        pprint(batch)
+        print()
         return
     assert batch.output_file_id is not None
 
@@ -201,6 +194,16 @@ def download(
     input_pairs = pl.read_parquet(batch_dir / "input.parquet")
     results = input_pairs.join(results, on=["speech_1_id", "speech_2_id"])
     results.write_parquet(batch_dir / "results.parquet")
+
+
+@app.command()
+def download(
+    batch_dirs: Annotated[
+        list[Path], typer.Argument(exists=True, file_okay=False, dir_okay=True)
+    ],
+):
+    for batch_dir in batch_dirs:
+        download_batch(batch_dir)
 
 
 if __name__ == "__main__":
